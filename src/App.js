f5268cd4cb52473d80cbb0d17b1b402ad898a64e
NavidManiAsl <navid.maniasl@outlook.com>
2022-09-03 17:52:04 +0200

    create navigation bar

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navigation from './components/Navigation';
+import 'bootstrap/dist/css/bootstrap.min.css'
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {Navbar} from 'react-bootstrap'
+import {Nav} from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
+import {navigationLinks} from '../helpers/navigationLinks'
+import '../styles/navigation.css'
+
+
+function Navigation() {
+  return (
+    <div id='navbar'>
+      <Container className='navigation__container'>  
+        <Navbar expand='md'>
+            <Navbar.Brand href='#home'>Navid Mani Asl</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse  style={{justifyContent: 'flex-end'}}>
+              <Nav>
+              {navigationLinks.map(link =>(
+                <Nav.Link href={link.ref} key={link.name}>{link.name}</Nav.Link>
+              ))}
+              </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+      </Container>  
+    </div>
+  )
+}
+
+export default Navigation
