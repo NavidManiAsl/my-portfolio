3ce9124a86bc9e881c47803a306d00dc0874825d
NavidManiAsl <navid.maniasl@outlook.com>
2022-09-04 06:47:21 +0200

    fix headers layershift

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,25 +7,26 @@ function Header() {
   return (
     <div className='header'>
         <img className='header__image' src={require( "../images/myPicture.png")} alt="Photo of Navid" />
-        <TypeAnimation
-    // Same String at the start will only be typed once, initially
-    sequence={[
-    'Who am i?',
-    1500,
-    'Who am I?',
-    1500,
-    'Passionate coder',
-    1000,
-    'Full Stack Developer',
-    1000,
-    'material science engineer',
-    1000,
-    ]}
-    speed={40} // Custom Speed from 1-99 - Default Speed: 40
-    style={{ fontSize: '2em' }}
-    wrapper="span" // Animation will be rendered as a <span>
-    repeat={Infinity} // Repeat this Animation Sequence infinitely
-  />
+        <div style={{width:400}}>
+            <TypeAnimation
+            sequence={[
+            'Who am i?',
+            1500,
+            'Who am I?',
+            1500,
+            'Passionate coder',
+            1000,
+            'Full Stack Developer',
+            1000,
+            'material science engineer',
+            1000,
+            ]}
+            speed={40} 
+            style={{ fontSize: '2em' }}
+            wrapper="span" 
+            repeat={Infinity} 
+        />
+        </div>
     </div>
   )
 }
