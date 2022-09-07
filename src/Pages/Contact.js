2266a3ea88abe48203c5a2df8c562388198b70dc
NavidManiAsl <navid.maniasl@outlook.com>
2022-09-07 22:02:13 +0200

    add mail functionality

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React,{useRef} from 'react'
 import Footer from '../components/Footer'
 import Navigation from '../components/Navigation'
+import emailjs from 'emailjs-com';
 
 function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_6ufypff', 'template_v5egt98', form.current, 'Eu-XJz1TJgjxOYMGc')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (
     <div>
       <Navigation />
-      <p>contact</p>
+      <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
       <Footer />
     </div>
   )
