3d0f48af72c835d8a811837493b364117fd7d8b1
NavidManiAsl <navid.maniasl@outlook.com>
2022-09-08 20:20:28 +0200

    redeployment

--- src/components/Construction.js
+++ src/components/Construction.js
@@ -5,7 +5,7 @@ import '../styles/construction.css'
 function Construction() {
   return (
     <div className='construction'>
-        <img src={require('../images/construction.png')} className='construction__image' />
+        <img src={require('../images/construction.png')} alt='' className='construction__image' />
         <p className='construction__text'>Under Construction</p>
     </div>
   )
